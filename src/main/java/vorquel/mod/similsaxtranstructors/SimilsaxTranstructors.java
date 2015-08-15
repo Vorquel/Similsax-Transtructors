be49a3ee7ace3db443663698862419679cc61782
Vorquel <vorquel@gmail.com>
2015-08-14 17:28:25 -0700

    Patch #3
    Now out for 1.8

--- src/main/java/vorquel/mod/similsaxtranstructors/SimilsaxTranstructors.java
+++ src/main/java/vorquel/mod/similsaxtranstructors/SimilsaxTranstructors.java
@@ -5,6 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -45,5 +46,14 @@ public class SimilsaxTranstructors {
                 " x ",
                 'x', Items.diamond,
                 'o', Items.ender_pearl);
+    }
+
+    @Mod.EventHandler
+    public void serverStarting(FMLServerStartingEvent event) {
+        ItemSimilsaxTranstructor item = SimilsaxTranstructors.itemSimilsaxTranstructor;
+        item.basicUses = Config.basicUses;
+        item.advancedUses = Config.advancedUses;
+        item.basicRange = Config.basicRange;
+        item.advancedRange = Config.advancedRange;
     }
 }
